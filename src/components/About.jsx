a13e0e9ced619c4b78e02d8788228d5cfdfd5fe4
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2024-12-26 09:38:37 +0530

    date

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ function About() {
             <h2>Full Stack developer</h2>
             <p>I'm  a Full Stack Developer with experience in designing and developing scalable web applications. Over the past 8 months, I have worked extensively with the MERN stack (MongoDB, Express.js, ReactJS, Node.js), creating efficient and responsive user interfaces and robust backend systems. My key skills include JavaScript, RESTful APIs, database management, and deployment. I approach development with attention to detail and a focus on clean, maintainable code. I enjoy collaborating with teams, adapting to challenges, and delivering solutions that meet both technical and user needs."</p>
             <div className='about_div11'>
-                <div> <span className='right_arr'><FaChevronRight /></span> Birthday:22 July 2023</div>
+                <div> <span className='right_arr'><FaChevronRight /></span> Birthday:22 July 2002</div>
                 <div>  <span className='right_arr'><FaChevronRight /></span>Phone:+91 6382688488</div>
                 <div> <span className='right_arr'><FaChevronRight /></span> City:Pondicherry, India</div>
                 <div> <span className='right_arr'><FaChevronRight /></span> Age:22</div>
